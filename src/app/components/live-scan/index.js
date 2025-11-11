4e04674694a185f009fec021fd1a93ad70245a4b
Imre Bernath <imre.h.bernath@gmail.com>
2025-11-11 08:13:30 +0100

    fix: remove Teun.ai dashboard and unused routes for clean OnlineLabs deploy

--- src/app/components/live-scan/index.js
+++
@@ -1,8 +0,0 @@
-// app/components/live-scan/index.js
-// Clean exports for the live-scan components
-
-export { default as LiveScanModal } from './LiveScanModal'
-
-// Future exports can be added here:
-// export { default as ScanHistory } from './ScanHistory'
-// export { default as ScanSettings } from './ScanSettings'
